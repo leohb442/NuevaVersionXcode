06208e0cd0eeefda6881daf4c37fbb27fa2fac6b
Leonardo Hernández Barrera <123115865+leohb442@users.noreply.github.com>
2023-04-18 13:54:42 -0600

    Add files via upload

---
+++ MiPrimerApp/MiPrimerApp/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  MiPrimerApp
+//
+//  Created by Facultad de Contaduría y Administración on 18/04/23.
+//
+
+import Foundation
---
+++ MiPrimerApp/MiPrimerApp/TableViewCellPerrito.swift
@@ -0,0 +1,23 @@
+//
+//  TableViewCellPerrito.swift
+//  MiPrimerApp
+//
+//  Created by Facultad de Contaduría y Administración on 18/04/23.
+//
+
+import UIKit
+
+class TableViewCellPerrito: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
